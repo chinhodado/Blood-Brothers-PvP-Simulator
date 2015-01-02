7d70a0c84bb90fd5c5a979759afc574aaba2b135
Chin <chin.bimbo@gmail.com>
2015-01-01 23:23:26 -0500

    Add familiar generator tool

--- tools/generate.js
+++ tools/generate.js
@@ -1,4 +1,8 @@
-﻿function generateSkill() {
+﻿/**
+ * Contains functions for generating contents for the skill and familiar database
+ */
+
+function generateSkill() {
     var lastId = +document.getElementById('skill').value;
     var content = "";
     for (var i = 0; i < srcdb.skills.length; i++) {
@@ -43,6 +47,51 @@
 
             skillTxt += ("\n    desc: \"" + skill.comment + "\"\n},");
             content += (skillTxt + "\n\n");
+        }
+    }
+
+    document.getElementById('result').innerText = content;
+}
+
+function generateFam() {
+    var names = document.getElementById('famList').value.split('\n');
+    var content = "";
+    for (var i = 0; i < names.length; i++) {
+        var name = names[i];
+        for (var j = 0; j < srcdb.cards.length; j++) { // O(n^2), but who cares...
+            var card = srcdb.cards[j];
+            if (card.name != name) continue;
+
+            // heuristic, of course
+            var shortName = name.split(" ").shift().replace(",", "");
+            content += (card.id + ": {\n" +
+                "    name: \"" + shortName + "\", stats: [" + card.popeHp + ", " + card.popeAtk + ", " +
+                card.popeDef + ", " + card.popeWis + ", " + card.popeAgi + "],\n");
+
+            var skills = "";
+            for (var skillIndex = 1; skillIndex <= 3; skillIndex++) {
+                if (card["skillId" + skillIndex] != 0) {
+                    skills += card["skillId" + skillIndex];
+                    if (card["skillId" + (skillIndex + 1)] != 0) {
+                        skills += ", ";
+                    }
+                } else {
+                    break; // assuming all empty skill slots are at the end
+                }
+            }
+
+            content += ("    skills: [" + skills + "],\n");
+
+            if (card.defaultSkillId != 0) {
+                content += ("    autoAttack: " + card.defaultSkillId  + ",\n");
+            }
+
+            if (card.cardType == 5) {
+                content += ("    isMounted: true,\n");
+            }
+
+            content += ("    img: \"foobar\",\n" +
+                "    fullName: \"" + name + "\"\n},\n");
         }
     }
 
