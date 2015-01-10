e2385dd83d3253114772cbfd27aeaa74cbaa46f2
Chin <chin.bimbo@gmail.com>
2015-01-09 20:56:25 -0500

    Use the saccable attribute to determine if a skill is available for select

--- src/skillProvider.ts
+++ src/skillProvider.ts
@@ -92,13 +92,6 @@
      * Return true if this skill should be available for user to select, or available to be randomly chosen
      */
     static isAvailableForSelect(skillId: number): boolean {
-        var isAvailable = true;
-        var skillInfo = SkillDatabase[skillId];
-
-        if (skillInfo.isAutoAttack || skillId == 355 || skillId == 452) {
-            isAvailable = false;
-        }
-
-        return isAvailable;
+        return SkillDatabase[skillId].sac == 1;
     }
 }
