277f5c77755d74bcdfe37629c1b0057d941052f4
Trung Do <chin.bimbo@gmail.com>
2019-01-16 22:54:04 -0500

    Remove lastTierUpdate stuffs

--- src/Helper.ts
+++ src/Helper.ts
@@ -164,20 +164,6 @@ function onFormLoad() {
     setPreviousChoices();
     toogleReserve();
     toogleDisable();
-
-    // display the last tier update time
-    if (localStorage.getItem("lastTierUpdateTime")) {
-        var a = new Date(+localStorage.getItem("lastTierUpdateTime"));
-        var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-        var year = a.getFullYear();
-        var month = months[a.getMonth()];
-        var date = a.getDate();
-        var hour = a.getHours();
-        var min = a.getMinutes();
-        var sec = a.getSeconds();
-        var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
-        $("#lastTierUpdate").text(`Last tier list update: ${time}`);
-    }
 }
 
 /**
